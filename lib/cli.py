210929576a3dd60ee83864d7aacf1542c5e0f24f
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-05-29 11:47:33 +0300

    Added a Review system

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,19 @@
 from lib.models.playlist import Playlist
 from lib.models.song import Song
+from lib.models.review import Review
 from lib.helpers import get_input, print_error
 
 def run_cli():
     playlist_model = Playlist()
     song_model = Song()
+    review_model = Review()
 
     while True:
         print("\nWelcome To The Main Menu! Choose an option")
         print("1. Playlists")
         print("2. Songs")
-        print("3. Exit")
+        print("3. Reviews")
+        print("4. Exit")
         choice = get_input("Choose an option: ")
 
         if choice == "1":
@@ -18,6 +21,8 @@ def run_cli():
         elif choice == "2":
             manage_songs(song_model)
         elif choice == "3":
+            manage_reviews(review_model)
+        elif choice == "4":
             print("Feel Free To Come Back!")
             break
         else:
@@ -85,6 +90,53 @@ def manage_songs(model):
             id_ = get_input("Song ID to delete: ")
             model.delete(id_)
             print("Song deleted.")
+        elif choice == "5":
+            break
+        else:
+            print_error("Invalid Choice! Choose a number in the choices given!")
+
+def manage_reviews(model):
+    while True:
+        print("\nReviews")
+        print("1. Create Review")
+        print("2. View All Reviews")
+        print("3. Update Review")
+        print("4. Delete Review")
+        print("5. Back")
+        choice = get_input("Choose an option: ")
+
+        if choice == "1":
+            review_text = get_input("Review text: ")
+            rating = get_input("Rating (1-5): ")
+            print("Review for:")
+            print("1. Playlist")
+            print("2. Song")
+            review_type = get_input("Choose an option: ")
+            playlist_id = None
+            song_id = None
+            if review_type == "1":
+                playlist_id = get_input("Playlist ID: ")
+            elif review_type == "2":
+                song_id = get_input("Song ID: ")
+            else:
+                print_error("Invalid choice for review type.")
+                continue
+            model.create(review_text, int(rating), playlist_id, song_id)
+            print("Review created.")
+        elif choice == "2":
+            reviews = model.get_all()
+            for r in reviews:
+                print(f"ID: {r[0]}, Review: {r[1]}, Rating: {r[2]}, Playlist ID: {r[3]}, Song ID: {r[4]}")
+        elif choice == "3":
+            id_ = get_input("Review ID to update: ")
+            new_review_text = get_input("New review text: ")
+            new_rating = get_input("New rating (1-5): ")
+            model.update(id_, new_review_text, int(new_rating))
+            print("Review updated.")
+        elif choice == "4":
+            id_ = get_input("Review ID to delete: ")
+            model.delete(id_)
+            print("Review deleted.")
         elif choice == "5":
             break
         else:
---
+++ lib/models/review.py
@@ -0,0 +1,39 @@
+from lib.models import CONN, CURSOR
+
+class Review:
+    def __init__(self):
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS reviews (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                review_text TEXT NOT NULL,
+                rating INTEGER NOT NULL,
+                playlist_id INTEGER,
+                song_id INTEGER,
+                FOREIGN KEY (playlist_id) REFERENCES playlists(id),
+                FOREIGN KEY (song_id) REFERENCES songs(id)
+            )
+        """)
+        CONN.commit()
+
+    def create(self, review_text, rating, playlist_id=None, song_id=None):
+        CURSOR.execute("""
+            INSERT INTO reviews (review_text, rating, playlist_id, song_id)
+            VALUES (?, ?, ?, ?)
+        """, (review_text, rating, playlist_id, song_id))
+        CONN.commit()
+
+    def get_all(self):
+        CURSOR.execute("SELECT * FROM reviews")
+        return CURSOR.fetchall()
+
+    def update(self, review_id, new_review_text, new_rating):
+        CURSOR.execute("""
+            UPDATE reviews
+            SET review_text = ?, rating = ?
+            WHERE id = ?
+        """, (new_review_text, new_rating, review_id))
+        CONN.commit()
+
+    def delete(self, review_id):
+        CURSOR.execute("DELETE FROM reviews WHERE id = ?", (review_id,))
+        CONN.commit()
